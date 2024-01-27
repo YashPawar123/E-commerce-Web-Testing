f56bfe43ff7fc01289ff83e62ce4461d38f8e6ae
YashPawar123 <pawaryash1215@gmail.com>
2024-01-27 20:32:42 +0530

    validation of moving product from cart to wishlist

---
+++ AMZ_IT_12.java
@@ -0,0 +1,40 @@
+package amazontestcases_new;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+public class AMZ_IT_12 {
+     @Test    
+	public void carttowishlist() {
+    	 ChromeDriver driver=new ChromeDriver();
+  		driver.manage().window().maximize();
+  		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+  		driver.get("https://www.amazon.in");
+  		
+  		WebElement account_button=driver.findElement(By.xpath("//span[.='Hello, sign in']"));
+  		Actions a1=new Actions(driver);
+  		a1.moveToElement(account_button);
+  		
+  		WebElement singin=driver.findElement(By.linkText("Sign in"));
+  		singin.click();
+  		
+  		WebElement un=driver.findElement(By.xpath("//input[@id='ap_email']"));
+  		un.sendKeys("yash.pawar1512@gmail.com");
+  		un.sendKeys(Keys.ENTER);
+  		
+  		WebElement pass=driver.findElement(By.xpath("//input[@id='ap_password']"));
+  		pass.sendKeys("Yash");
+  		pass.sendKeys(Keys.ENTER);
+  		
+  		driver.findElement(By.xpath("(//span[@class='nav-line-2'])[3]")).click();
+  		driver.findElement(By.xpath("(//input[@class='a-color-link'])[2]")).click();
+  		driver.findElement(By.xpath("( //span[@class='a-size-mini a-color-secondary cldd-list-privacy'])[2]")).click();
+  		
+	}
+}
