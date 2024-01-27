742caae0a5a3f73c30ebf4e499fc358da1ce40e7
YashPawar123 <pawaryash1215@gmail.com>
2024-01-27 20:02:09 +0530

    validation of the address page

---
+++ AMZ_IT_07.java
@@ -0,0 +1,37 @@
+package amazontestcases_new;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+public class AMZ_IT_07 {
+    @Test
+	public void addresspage() {
+    	ChromeDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://www.amazon.in");
+		WebElement account_button=driver.findElement(By.xpath("//span[.='Hello, sign in']"));
+		Actions a1=new Actions(driver);
+		a1.moveToElement(account_button);
+		WebElement singin=driver.findElement(By.linkText("Sign in"));
+		singin.click();
+		WebElement un=driver.findElement(By.xpath("//input[@id='ap_email']"));
+		un.sendKeys("yash.pawar1512@gmail.com");
+		un.sendKeys(Keys.ENTER);
+		WebElement pass=driver.findElement(By.xpath("//input[@id='ap_password']"));
+		pass.sendKeys("*****");
+		pass.sendKeys(Keys.ENTER);
+		
+		WebElement sin=driver.findElement(By.xpath("//span[.='Hello, yash']"));
+		sin.click();
+		
+		WebElement address=driver.findElement(By.xpath("//span[.='Edit addresses for orders and gifts']"));
+		address.click();
+	}
+} 
